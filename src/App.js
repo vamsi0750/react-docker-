6b2ecac99212ca103ac93df95231f4a2ae0eb95f
vamsi praveen <vamsi>
2020-01-08 03:15:23 +0530

    added app js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          vamsi krishna nagisetty
-        </p>
-        
+        <p>An amazing startup idea !!.</p>
+        <p>Assignment done. LearnCodeOnline wooo</p>
+        <p>Instagram: https://instagram.com/hiteshChoudharyOfficial</p>
       </header>
     </div>
   );
